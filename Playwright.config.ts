020e13f5733c8e30a3e7e0db1d97592e13bd2239
Bladimir <oirbl@hotmail.com>
2026-03-16 22:05:32 -0600

    fix(project): remove comments in pr workflow

--- Playwright.config.ts
+++ Playwright.config.ts
@@ -1,34 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: "./tests",
+  testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [
-    ["html", { outputFolder: "playwright-report", open: "never" }],
-    ["list"],
-    ["junit", { outputFile: "test-results/results.xml" }],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['list'],
+    ['junit', { outputFile: 'test-results/results.xml' }],
   ],
   use: {
-    baseURL: "https://practicetestautomation.com",
-    trace: "on-first-retry",
-    screenshot: "only-on-failure",
-    video: "on-first-retry",
+    baseURL: 'https://practicetestautomation.com',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'on-first-retry',
     actionTimeout: 10_000,
     navigationTimeout: 30_000,
   },
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
     },
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Edge"] },
-    // },
   ],
-  outputDir: "test-results/",
+  outputDir: 'test-results/',
 });
